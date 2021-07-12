74952da2c184e6b802378bf12940dd197368e1df
Majorka93 <Zaikina1993>
2021-07-12 10:12:32 +0300

    добавили параметры абонентов

---
+++ src/main/java/Subscriberparameters.java
@@ -0,0 +1,18 @@
+public class Subscriberparameters {
+
+    private String name;
+    private String surname;
+    private String birthday;
+    private long phoneNumber;
+
+    public Subscriberparameters(String name, String surname, String birthday, long phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.phoneNumber = phoneNumber;
+
+
+    }
+
+
+}
