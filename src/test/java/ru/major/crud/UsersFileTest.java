a0da722db63165c613d7a260a70383abf8b154be
Majorka93 <Zaikina1993>
2021-07-27 20:21:05 +0300

    Написаны тесты для методов удаления и обновления контакта.

--- src/test/java/ru/major/crud/UsersFileTest.java
+++ src/test/java/ru/major/crud/UsersFileTest.java
@@ -2,7 +2,9 @@ package ru.major.crud;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -12,7 +14,7 @@ public class UsersFileTest {
 
     @Test
     public void findAllContactsTest() throws IOException {
-        UsersFile repository = new UsersFile("C:\\Users\\Integro\\IdeaProjects\\CRUD\\src\\test\\resources\\db1.txt");
+        UsersFile repository = findFilePath("db1.txt");
         List<Contact> allContacts = repository.findAllContacts();
         assertEquals(3, allContacts.size());
         assertEquals("Иван", allContacts.get(0).getName());
@@ -20,11 +22,38 @@ public class UsersFileTest {
 
     @Test
     public void findByPhoneNumberTest() throws IOException {
-        UsersFile repository = new UsersFile("C:\\Users\\Integro\\IdeaProjects\\CRUD\\src\\test\\resources\\db1.txt");
-        List<Contact> allContacts = repository.selectByPhone(79047778811L);
+        UsersFile repository = findFilePath("db1.txt");
+        List<Contact> allContacts = repository.selectByPhone("+79047778811");
         assertEquals(1, allContacts.size());
         assertEquals("Алеша", allContacts.get(0).getName());
         assertEquals("Алешков", allContacts.get(0).getSurname());
     }
 
+    @Test
+    public void deleteContactTest() throws IOException {
+//todo Тесты проверить в 2 этапа. Сначала проверяем начальное состояние, потом проверяем результат
+        UsersFile repository = findFilePath("SubscribersTest.txt");
+        List<Contact> allContacts = repository.findAllContacts();
+        repository.deleteContact("+79996660000");
+        assertEquals(4, repository.findAllContacts().size());
+    }
+
+    @Test
+    public void updateContactTest() throws IOException {
+        UsersFile repository = findFilePath("SubscribersTest.txt");
+        repository.updateContact("+79046741611", "Сергей", "Жириновский", "01.02.1945");
+        List<Contact> allContacts = repository.findAllContacts();
+        assertEquals("Сергей", allContacts.get(3).getName());
+        assertEquals("Жириновский", allContacts.get(3).getSurname());
+        assertEquals("01.02.1945", allContacts.get(3).getBirthday());
+    }
+
+    private UsersFile findFilePath(String fileName) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        return new UsersFile(file.getAbsolutePath());
+
+    }
 }
